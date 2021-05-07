5199a01c99baa355fe6f3996a16c4288b9016c01
hedi-code <hedibensafegine7@gmail.com>
2021-05-08 00:13:29 +0300

    validators

--- src/app/components/update-user/update-user.component.ts
+++ src/app/components/update-user/update-user.component.ts
@@ -11,36 +11,44 @@ import { SnackbarServiceService } from 'src/app/services/snackbar-service.servic
   styleUrls: ['./update-user.component.scss']
 })
 export class UpdateUserComponent implements OnInit {
+  currentUserValues: User = new User();
+
   updateUserForm: FormGroup = new FormGroup({
-    last_name: new FormControl("", [Validators.required]),
-    first_name: new FormControl("", [Validators.required]),
-    email: new FormControl("", [Validators.required]),
-    role: new FormControl("", [Validators.required]),
+    last_name: new FormControl(""),
+    first_name: new FormControl(""),
+    email: new FormControl(""),
+    role: new FormControl(""),
     id: new FormControl("")
   });
-  currentUserValues: User = new User();
   constructor(private userService: UserServiceService, @Inject(MAT_DIALOG_DATA) public data: number, private snackbarService: SnackbarServiceService) {
+    this.userService.getUser(this.data).subscribe(result => {
+      console.warn(result)
+      this.currentUserValues.lastname = result.data.lastname;
+      this.currentUserValues.firstname = result.data.firstname;
+      this.currentUserValues.role = result.data.role;
+      this.currentUserValues.email = result.data.email;
 
+      this.updateUserForm = new FormGroup({
+        last_name: new FormControl(result.data.lastname, [Validators.required, Validators.minLength(5)]),
+        first_name: new FormControl(result.data.firstname, [Validators.required]),
+        email: new FormControl(result.data.email, [Validators.required, Validators.email]),
+        role: new FormControl(result.data.role, [Validators.required]),
+        id: new FormControl("")
+      });
+
+
+      console.log(this.currentUserValues.lastname + "aaaaaaaaaaaa")
+
+
+    })
   }
 
   ngOnInit() {
 
-    this.initForm();
   }
-  initForm() {
-    this.userService.getUser(this.data).subscribe(result => {
-      this.currentUserValues = result.data;
-      this.updateUserForm.value.first_name = result.data.firstname;
-      this.updateUserForm.value.last_name = result.data.lastname;
-      this.updateUserForm.value.email = result.data.email;
-      this.updateUserForm.value.role = result.data.role;
 
-
-      console.log(this.currentUserValues.firstname + "aaaaaaaaaaaa")
-
-
-    })
-
+  get f() {
+    return this.updateUserForm.controls
   }
 
   updateUser() {
